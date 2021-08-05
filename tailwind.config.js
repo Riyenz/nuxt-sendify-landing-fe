055395f0e7d1876b421ab5e56c5ce1eb576929bb
Riyenz <risotiong@gmail.com>
2021-08-05 11:28:03 +0800

    feat: make the colors and fonts as extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   theme: {
-    fontFamily: {
-      sans: ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system'],
-    },
-    colors: {
-      ...colors,
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system'],
+      },
+      colors: {
+        ...colors,
+      },
     },
   },
   variants: {
